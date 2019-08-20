424a56aa72381e1d71a406e0751eefbdf77b4b34
XX <xx@XXs-MBP13.local>
2019-08-20 19:21:17 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>The Beginning of Something</h1>
+        </header>
+
+       
+      </div>
+    );
+  }
+}
+
+export default App;
