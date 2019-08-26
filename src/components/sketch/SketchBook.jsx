5c6f5fa7df20939f38d74e838f682439cc494269
XX <fallwall19@gmail.com>
2019-08-26 11:26:19 -0400

    sketch page importing

--- src/components/sketch/SketchBook.jsx
+++ src/components/sketch/SketchBook.jsx
@@ -4,26 +4,28 @@ import { Fade } from 'react-reveal';
 import { Image } from 'semantic-ui-react';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
+
 export default class SketchBook extends Component {
   render() {
-    const designURLS = ['design1.jpg', 'design2.jpg', 'design3.jpg', 'design4.jpg'];
+    const designURLS = ['pic1.jpg', 'pic2.jpg', 'pic1.jpg', 'pic2.jpg'];
     const designs = designURLS.map((design, i) => {
       return (
 
         <Image
           src={design}
           className='design-thumb'
-          id={'design-' + i}
+          id={i}
           size='small'
-          onClick={props.handleDesignModal}
+        // onClick={props.handleDesignModal}
         />
       )
     });
 
     return (
-      <Fade left opposite when={props.showDesign} >
+      // <Fade left opposite when={props.showDesign} >
+      <Fade left opposite >
         <div className='design'>
-          <h1>design</h1>
+          <h1>Sketch</h1>
           <ScrollableAnchor id={'design'}>
             <div className='design-header'>
               <p>I was asked to write instruction for my successor in previous job and I drew 20 pages instead.</p>
@@ -31,12 +33,13 @@ export default class SketchBook extends Component {
             </div>
           </ScrollableAnchor>
           <div className='design-port'>
-            <Fade left cascade when={props.showDesign} >
+            {/* <Fade left cascade when={props.showDesign} > */}
+            <Fade left cascade >
               {designs}
             </Fade>
           </div>
           <div className='design-footer'>
-            <p>I spent seven years of my life drawing on weekends. It was nothing.</p>
+            <p>Music inspired.</p>
           </div>
         </div>
       </Fade>
