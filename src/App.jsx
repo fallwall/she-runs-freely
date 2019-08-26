b92ab53fce808de99a6b04a898735effa8dd8dba
XX <fallwall19@gmail.com>
2019-08-26 16:21:38 -0400

    moved parallax banner to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import Scroll from './components/Scroll';
-import Title from './components/Title';
+
 import Projects from './components/projects/Projects';
 import SketchBook from './components/sketch/SketchBook';
 import About from './components/about/About';
@@ -10,8 +9,7 @@ import About from './components/about/About';
 import Home from './components/Home';
 import './App.css';
 import Slide from 'react-reveal/Slide';
-import { Fade } from 'react-reveal';
-import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 
@@ -75,48 +73,18 @@ class App extends React.Component {
             <Nav />
           </header>
 
-          <Route path="/" render={() => <Home />} />
+          <Route path="/" render={() => <Home
+             loaded={this.state.loaded}
+             handleModal={this.handleModal}
+             handleResume={this.handleResume}
+             showScroll={this.state.showScroll} 
+             showName={this.state.showName}
+          />} />
           <Route path="/projects" render={() => <Projects />} />
           <Route path="/sketch" render={() => <SketchBook />} />
           <Route path="/about" render={() => <About />} />
 
-          <div className='splash-page'>
-            {/* <Fade clear when={!props.loaded} > */}
-            <div className='load-screen'>
-            </div>
-            {/* </Fade> */}
-            <ParallaxBanner
-              className='splash-parallax'
-              layers={[
-                {
-                  image: '/teton-11.png',
-                  amount: 0,
-                }, {
-                  image: '/teton2.png',
-                  amount: 0.6,
-                }, {
-                  image: '/teton3.png',
-                  amount: 0.2,
-                }, {
-                  image: '',
-                  amount: 0.1,
-                  children: <div className='scroll'>
-                    <Scroll showScroll={this.state.showScroll} />
-                  </div>
-                }, {
-                  image: '',
-                  amount: 0.9,
-                  children: <>
-                    <Title showName={this.state.showName} />
-                  </>,
-                }
-              ]}
-              style={{
-                height: '700px',
-              }}
-            >
-            </ParallaxBanner>
-          </div>
+         
 
           <div className="home-page-container">
             <div className="home-page">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Scroll from './Scroll';
+import Title from './Title';
+import { Fade } from 'react-reveal';
+import { ParallaxBanner } from 'react-scroll-parallax';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        
+export default function Home(props) {
+
+  return (
+    <div>
+      <div className='splash-page'>
+        <Fade clear when={!props.loaded} >
+          <div className='load-screen'>
+          </div>
+        </Fade>
+        <ParallaxBanner
+          className='splash-parallax'
+          layers={[
+            {
+              image: '/teton-11.png',
+              amount: 0,
+            }, {
+              image: '/teton2.png',
+              amount: 0.6,
+            }, {
+              image: '/teton3.png',
+              amount: 0.2,
+            }, {
+              image: '',
+              amount: 0.1,
+              children: <div className='scroll'>
+                <Scroll showScroll={props.showScroll} />
+              </div>
+            }, {
+              image: '',
+              amount: 0.9,
+              children: <>
+                <Title showName={props.showName} />
+              </>,
+            }
+          ]}
+          style={{
+            height: '700px',
+          }}
+        >
+        </ParallaxBanner>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
